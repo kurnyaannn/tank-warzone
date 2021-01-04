7f3546703a0b2a8d3791fefac0a1616f2aa42b9d
kwrnya <y21kurnia@gmail.com>
2021-01-05 06:31:17 +0700

    init

---
+++ tanks/Player.gd
@@ -0,0 +1,20 @@
+extends "res://tanks/Tank.gd"
+
+func control(delta):
+	$Turret.look_at(get_global_mouse_position())
+	var rot_dir = 0
+	if Input.is_action_pressed('turn_right'):
+		rot_dir += 1
+	if Input.is_action_pressed('turn_left'):
+		rot_dir -= 1
+	rotation += rotation_speed * rot_dir * delta
+	velocity = Vector2()
+	if Input.is_action_pressed('forward'):
+		velocity += Vector2(max_speed, 0).rotated(rotation)
+	if Input.is_action_pressed('back'):
+		velocity += Vector2(-max_speed, 0).rotated(rotation)
+		velocity /= 2.0
+	if Input.is_action_just_pressed('click'):
+		shoot()
+	position.x = clamp(position.x, $Camera2D.limit_left, $Camera2D.limit_right)
+	position.y = clamp(position.y, $Camera2D.limit_top, $Camera2D.limit_bottom)
