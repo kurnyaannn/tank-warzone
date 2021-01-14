4cbc36d2aa65dfe45d296e7ea8a08f7b8328321d
kwrnya <y21kurnia@gmail.com>
2021-01-14 20:50:34 +0700

    adding title screen, assets, level, bullet

--- maps/Map.gd
+++ maps/Map.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _ready():
 	set_camera_limits()
 	Input.set_custom_mouse_cursor(load("res://assets/UI/crossair_black.png"), Input.CURSOR_ARROW, Vector2(16, 16))
+	$Player.map = $Ground
 
 func set_camera_limits():
 	var map_limits = $Ground.get_used_rect()
@@ -12,10 +13,11 @@ func set_camera_limits():
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
 	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
 
-func _on_Tank_shoot(bullet, _position, _direction):
+func _on_Tank_shoot(bullet, _position, _direction, _target=null):
 	var b = bullet.instance()
 	add_child(b)
-	b.start(_position, _direction)
+	b.start(_position, _direction, _target)
 
 func _on_Player_dead():
-	get_tree().reload_current_scene()
+	GLOBALS.restart()
+
--- tanks/Tank.gd
+++ tanks/Tank.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 signal shoot
 signal health_changed
+signal ammo_changed
 signal dead
 
 export (PackedScene) var Bullet
@@ -9,40 +10,68 @@ export (int) var max_speed
 export (float) var rotation_speed
 export (float) var gun_cooldown
 export (int) var max_health
+export (float) var offroad_friction
+
+export (int) var gun_shots = 1
+export (float, 0, 1.5) var gun_spread = 0.2
+export (int) var max_ammo = 20
+export (int) var ammo = -1 setget set_ammo
 
 var velocity = Vector2()
 var can_shoot = true
 var alive = true
 var health
+var map
 
 func _ready():
 	health = max_health
+	$Smoke.emitting = false
 	emit_signal('health_changed', health * 100/max_health)
+	emit_signal('ammo_changed', ammo * 100/max_ammo)
 	$GunTimer.wait_time = gun_cooldown
 
 func control(delta):
 	pass
 
-func shoot():
-	if can_shoot:
+func shoot(num, spread, target=null):
+	if can_shoot and ammo != 0:
+		self.ammo -= 1
 		can_shoot = false
 		$GunTimer.start()
 		var dir = Vector2(1, 0).rotated($Turret.global_rotation)
-		emit_signal('shoot', Bullet, $Turret/Muzzle.global_position, dir)
+		if num > 1:
+			for i in range(num):
+				var a = -spread + i * (2*spread)/(num-1)
+				emit_signal('shoot', Bullet, $Turret/Muzzle.global_position, dir.rotated(a), target)
+		else:
+			emit_signal('shoot', Bullet, $Turret/Muzzle.global_position, dir, target)
 		$AnimationPlayer.play('muzzle_flash')
 
 func _physics_process(delta):
 	if not alive:
 		return
 	control(delta)
+	if map:
+		var tile = map.get_cellv(map.world_to_map(position))
+		if tile in GLOBALS.slow_terrain:
+			velocity *= offroad_friction
 	move_and_slide(velocity)
 
 func take_damage(amount):
 	health -= amount
 	emit_signal('health_changed', health * 100/max_health)
+	if health < max_health / 2:
+		$Smoke.emitting = true
 	if health <= 0:
 		explode()
 
+func heal(amount):
+	health += amount
+	health = clamp(health, 0, max_health)
+	emit_signal('health_changed', health * 100/max_health)
+	if health >= max_health / 2:
+		$Smoke.emitting = false
+	
 func explode():
 	$CollisionShape2D.disabled = true
 	alive = false
@@ -54,6 +83,12 @@ func explode():
 
 func _on_GunTimer_timeout():
 	can_shoot = true
-
+	
 func _on_Explosion_animation_finished():
 	queue_free()
+	
+func set_ammo(value):
+	if value > max_ammo:
+		value = max_ammo
+	ammo = value
+	emit_signal('ammo_changed', ammo * 100/max_ammo)
